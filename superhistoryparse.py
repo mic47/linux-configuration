38fa09a4bb97bc7cb5b8b91f884efa1f3a9265e6
Michal Nánási <michal.nanasi@gmail.com>
2020-03-26 23:28:40 +0100

    Stuff

---
+++ superhistoryparse.py
@@ -0,0 +1,24 @@
+import sys
+
+token = sys.argv[1]
+pwd = sys.argv[2]
+
+order= [[], [], []]
+for line in sys.stdin:
+    try:
+        with_token, _, _, _, directory, command = line.split(",", maxsplit=5)
+        _, hist_token = line.rsplit(" ", maxsplit=1)
+        if hist_token == token:
+            order[2].append(command)
+        elif directory == pwd or (" " in directory and directory.startswith(pwd)):
+            order[1].append(command)
+        else:
+            order[0].append(command)
+    except ValueError:
+        pass
+
+for x in order:
+    for y in x:
+        sys.stdout.write(y)
+
+
