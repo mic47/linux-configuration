b94539885d4f2554faa8b4d4f5bd40f462b9eb0c
Michal Nánási <michal.nanasi@gmail.com>
2020-03-26 23:46:23 +0100

    Faster

--- superhistoryparse.cpp
+++ superhistoryparse.cpp
@@ -5,14 +5,25 @@
 
 using namespace std;
 
-bool is_directory(char* directory, char* current_path) {
+inline bool is_directory(char* directory, char* current_path) {
     for(;*directory != 0 && *current_path != 0 && *directory == *current_path; directory++, current_path++);
     if (*current_path != 0) return false;
     if (*directory == 0) return true;
     return strncmp(directory, " -> ", 4) == 0;
 }
 
-#define BAILOUT if(*input_line==0){input_line=NULL;n=0;continue;}
+#define BAILOUT if(*input_line==0){input_line=NULL;continue;}
+
+inline bool skip_over_column_and_found_end_of_string(char* &input_line) {
+  for(;*input_line != 0 && *input_line != ','; ++input_line);
+  if (*input_line == 0) {
+    input_line = NULL;
+    return true;
+  }
+  ++input_line;
+  return false;
+}
+
 
 int main(int argc, char* argv[]) {
     if (argc < 3) {
@@ -26,28 +37,25 @@ int main(int argc, char* argv[]) {
 
     size_t n=0;
     while(getline(&input_line, &n, stdin) >= 0) {
+        n = 0;
         char *token, *directory, *command;
         auto last_start = input_line;
-        while(*input_line > 0 && *input_line != ',') {
+        while(*input_line != 0 && *input_line != ',') {
             if (*input_line == ' ') last_start = input_line + 1;
             ++input_line;
         }
         BAILOUT;
         *input_line=0;
         token=last_start;
-        for(last_start=++input_line;*input_line > 0 && *input_line != ','; ++input_line);
-        BAILOUT;
-        for(last_start=++input_line;*input_line > 0 && *input_line != ','; ++input_line);
-        BAILOUT;
-        for(last_start=++input_line;*input_line > 0 && *input_line != ','; ++input_line);
-        BAILOUT;
-        for(last_start=++input_line;*input_line > 0 && *input_line != ','; ++input_line);
-        BAILOUT;
+        if (skip_over_column_and_found_end_of_string(input_line)) continue;
+        if (skip_over_column_and_found_end_of_string(input_line)) continue;
+        if (skip_over_column_and_found_end_of_string(input_line)) continue;
+        last_start = input_line;
+        if (skip_over_column_and_found_end_of_string(input_line)) continue;
         directory=last_start;
         *input_line=0;
         command=input_line+1;
         input_line = NULL;
-        n=0;
         if (strcmp(token, argv[1]) == 0) {
             outputs[2].push_back(command);
         } else if (is_directory(directory, argv[2])) {
