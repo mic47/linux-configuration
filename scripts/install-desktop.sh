1793cb0692a0fe81ac9b4cf8e1de5e718da3aa39
Michal Nánási <michal.nanasi@gmail.com>
2021-09-19 21:24:22 +0200

    fix install

--- scripts/install-desktop.sh
+++ scripts/install-desktop.sh
@@ -16,7 +16,7 @@ set -ex
 # [x] albert plugins
 # [x] /home/mic/Pictures/i3wm_reference.png
 # [x] /home/mic/Pictures/macros/kubernetes_guide.png
-# [ ] ~/.local/bin/i3status-custom.sh
+# [x] ~/.local/bin/i3status-custom.sh
 # [ ] /home/mic/.local/bin/terminalNotes.sh
 
 sudo apt-get install curl wget
@@ -25,7 +25,6 @@ curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -
 echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
-sudo wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key -O "/etc/apt/trusted.gpg.d/home:manuelschneid3r.asc"
 
 sudo apt update
 sudo apt-get install \
@@ -71,11 +70,11 @@ mkdir -p ~/.local/bin
 ln -s "$(pwd)/desktop/i3status-custom.sh" ~/.local/bin/i3status-custom.sh
 mkdir -p ~/.config/i3status
 ln -s "$(pwd)/desktop/i3status.config" ~/.config/i3status/config
-rm -r ~/.local/share/albert/org.albert.extension.python/modules
+rm -rf ~/.local/share/albert/org.albert.extension.python/modules
 mkdir -p ~/.local/share/albert/org.albert.extension.python
 ln -s "$(pwd)/desktop/albert/python_plugins" ~/.local/share/albert/org.albert.extension.python/modules
 mkdir -p ~/.config/albert
-rm ~/.config/albert/albert.conf
+rm -f ~/.config/albert/albert.conf
 ln -s "$(pwd)/desktop/albert/albert.conf" ~/.config/albert/albert.conf
 
 mkdir ~/.logs
